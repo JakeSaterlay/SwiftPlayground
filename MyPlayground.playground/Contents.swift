10c81b30a08344336f936d0e664db0f67acc0f95
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2026-04-05 11:08:36 +0100

    variables and functions

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var greeting = "Hello, playground"
-
-func square(val: Int) -> Int {
-    return val * val;
-}
-
-square(val: 5)
---
+++ MyPlayground.playground/Pages/Basic Expressions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+
+// spaces and new lines are usually ignored
+var x = 1; var y = 2;
+
+// boolean conditions dont need parenthesis
+if x > 1 {
+    
+}
+
+
