591368ddf80b7e41a58d59c98b5fe7748151c23f
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2026-04-06 15:29:14 +0100

    more on variables

--- MyPlayground.playground/Pages/Basic Expressions.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Basic Expressions.xcplaygroundpage/Contents.swift
@@ -21,3 +21,21 @@ class ClassB {
 
 var myBClass = ClassB();
 print(myBClass.test())
+
+// let is the way to declare constants
+let tax = 4.5
+//tax = 4.0 WILL ERROR
+
+// Typing
+let name: String;
+name = "hello"
+// name = 34 // WILL ERROR
+
+
+// Template literal
+let myNumber = 4
+var message = "The number is \(myNumber)"
+
+// Tuples
+let city:(String, Int) = ("Norwich", 10000)
+print(city.0, city.1)
